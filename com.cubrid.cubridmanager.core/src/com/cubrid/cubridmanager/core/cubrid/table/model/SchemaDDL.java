9c39e2ef3be2364e27d26ac5f0df361e0c75e8a6
seunghun <seunghun_kim@cubrid.com>
2016-03-15 17:48:09 +0900

    [RND-4][bug] schema compare wizard is make incorrect result when using auto_increment

--- com.cubrid.cubridmanager.core/src/com/cubrid/cubridmanager/core/cubrid/table/model/SchemaDDL.java
+++ com.cubrid.cubridmanager.core/src/com/cubrid/cubridmanager/core/cubrid/table/model/SchemaDDL.java
@@ -29,6 +29,8 @@
  */
 package com.cubrid.cubridmanager.core.cubrid.table.model;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -36,6 +38,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+
+import org.slf4j.Logger;
 
 import com.cubrid.common.core.common.model.Constraint;
 import com.cubrid.common.core.common.model.Constraint.ConstraintType;
@@ -46,11 +50,16 @@ import com.cubrid.common.core.common.model.PartitionType;
 import com.cubrid.common.core.common.model.SchemaInfo;
 import com.cubrid.common.core.common.model.SerialInfo;
 import com.cubrid.common.core.util.CompatibleUtil;
+import com.cubrid.common.core.util.LogUtil;
 import com.cubrid.common.core.util.PartitionUtil;
 import com.cubrid.common.core.util.QuerySyntax;
 import com.cubrid.common.core.util.StringUtil;
+import com.cubrid.cubridmanager.core.common.jdbc.DBConnection;
+import com.cubrid.cubridmanager.core.common.jdbc.JDBCConnectionManager;
 import com.cubrid.cubridmanager.core.cubrid.database.model.DatabaseInfo;
 import com.cubrid.cubridmanager.core.cubrid.table.model.SchemaChangeLog.SchemeInnerType;
+import com.cubrid.jdbc.proxy.driver.CUBRIDPreparedStatementProxy;
+import com.cubrid.jdbc.proxy.driver.CUBRIDResultSetProxy;
 
 /**
  * This class provide a serial of methods to generate DDL of a schema or alter
@@ -60,6 +69,7 @@ import com.cubrid.cubridmanager.core.cubrid.table.model.SchemaChangeLog.SchemeIn
  * @version 1.0 - 2009-5-22 created by moulinwang
  */
 public class SchemaDDL {
+	private static final Logger LOGGER = LogUtil.getLogger(SchemaDDL.class);
 	private String endLineChar = ";";
 	private final SchemaChangeManager changeLogMgr;
 	protected final DatabaseInfo databaseInfo;
@@ -2456,10 +2466,34 @@ public class SchemaDDL {
 	public String getAlterAutoIncrementDDL(String tableName, String columnName, String startWith,
 			String incrementby, String minValue) {
 		StringBuilder ddl = new StringBuilder();
-		String escapedSerialName = QuerySyntax.escapeKeyword(tableName + "_ai_" + columnName);
-		ddl.append("ALTER SERIAL ").append(escapedSerialName);
-		ddl.append(" START WITH ").append(startWith).append(" INCREMENT BY ").append(incrementby).append(
-				" MINVALUE ").append(minValue).append(endLineChar).append(StringUtil.NEWLINE);
+		long max = 0;
+		DBConnection connection = null;
+		CUBRIDResultSetProxy rs = null;
+		CUBRIDPreparedStatementProxy pStmt = null;
+		
+		try {
+			String sql = "SELECT MAX(" + QuerySyntax.escapeKeyword(columnName) + ") + 1 "
+					+ "FROM " + QuerySyntax.escapeKeyword(tableName);
+			connection = (DBConnection)JDBCConnectionManager.getConnection(databaseInfo, true);
+			pStmt = (CUBRIDPreparedStatementProxy)((Connection)connection).prepareStatement(sql);
+			rs = (CUBRIDResultSetProxy)pStmt.executeQuery();
+			if (rs.next()) max = rs.getLong(1);
+		} catch (SQLException e) {
+			LOGGER.error(e.getMessage(), e);
+		} finally {
+			try {
+				if (rs != null) rs.close();
+				if (pStmt != null) pStmt.close();
+				if (connection != null) connection.close();
+			} catch (SQLException e) {
+				LOGGER.error(e.getMessage(), e);
+			}
+		}
+		if (max > 0) {
+			ddl.append("ALTER TABLE ").append(QuerySyntax.escapeKeyword(tableName)).append(" AUTO_INCREMENT=" )
+			.append(max).append(endLineChar).append(StringUtil.NEWLINE);
+		}
+
 		return ddl.toString();
 	}
 }
